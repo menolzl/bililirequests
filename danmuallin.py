15b841e27216f1ea5876860650ab59e72d7f5e87
menolzl <48782434+menolzl@users.noreply.github.com>
2022-03-10 18:49:36 +0800

    Update danmuallin.py

--- danmuallin.py
+++ danmuallin.py
@@ -4,7 +4,7 @@ import json
 import re
 import datetime
 
-headers = {"Cookie":"buvid3=472C2412-7550-D2CF-70F4-A1DD208F295B66351infoc; i-wanna-go-back=-1; _uuid=986D515D-6176-79A4-28C8-F5105732F8C91065827infoc; buvid4=18C58428-3E9A-3118-941E-33F16F6E873A67496-022030823-9E/zTJJxAKuUmUVtWG83gQ%3D%3D; buvid_fp_plain=undefined; LIVE_BUVID=AUTO1816467533827294; blackside_state=1; rpdid=|(um|k~|J~kR0J'uYRY~lR|)k; CURRENT_QUALITY=0; CURRENT_BLACKGAP=0; PVID=1; fingerprint=6a1fdf384fa4103e094963ef3933bdb1; CURRENT_FNVAL=4048; b_lsid=DF25A12B_17F7265F6AB; b_ut=5; innersign=1; bsource=search_baidu; bp_video_offset_1379528=635831292200484900; buvid_fp=472C2412-7550-D2CF-70F4-A1DD208F295B66351infoc; SESSDATA=7afcfc58%2C1662446427%2C50c6f%2A31; bili_jct=09e38be67a3a9b0e69993dec4c979e12; DedeUserID=1379528; DedeUserID__ckMd5=e69363711611e319; sid=749yciex","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
+headers = {"Cookie":"","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
 
 
 def get_date():
@@ -15,7 +15,7 @@ def get_date():
         yield day
 
 def get_bvid():
-    data = pd.read_excel('D://桌面/b站原创top.xlsx',sheet_name='弹幕需求目录')
+    data = pd.read_excel('b站原创top.xlsx',sheet_name='弹幕需求目录')
     bvid = list(data['BVID'])
     return bvid
 
@@ -53,7 +53,7 @@ def parse_page(response):
 
 
 def save_data(data):
-    with open(f'D:/桌面/弹幕/b站top20弹幕1.txt', 'a',encoding="utf_8_sig") as f:
+    with open(f'弹幕/b站top20弹幕1.txt', 'a',encoding="utf_8_sig") as f:
         for i in data:
             f.write(i+'\n')
 
