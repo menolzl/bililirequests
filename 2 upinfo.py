6ffbcaa4e84356f21c18ce66ef4d48f243a91054
menolzl <48782434+menolzl@users.noreply.github.com>
2022-03-10 18:52:44 +0800

    Update and rename upinfo.py to 2 upinfo.py

--- 2 upinfo.py
+++ 2 upinfo.py
@@ -1,10 +1,10 @@
 import requests
 import pandas as pd
 
-headers = {"Cookie":"buvid3=472C2412-7550-D2CF-70F4-A1DD208F295B66351infoc; i-wanna-go-back=-1; _uuid=986D515D-6176-79A4-28C8-F5105732F8C91065827infoc; buvid4=18C58428-3E9A-3118-941E-33F16F6E873A67496-022030823-9E/zTJJxAKuUmUVtWG83gQ%3D%3D; fingerprint=dfac9c33c01ee95d803470847cb6af0d; buvid_fp_plain=undefined; buvid_fp=6a1fdf384fa4103e094963ef3933bdb1; SESSDATA=800d70de%2C1662305379%2C01502%2A31; bili_jct=a0450f50fe222d23a1a016452f04e5f9; DedeUserID=1379528; DedeUserID__ckMd5=e69363711611e319; sid=i7mi2e85; b_ut=5; LIVE_BUVID=AUTO1816467533827294; blackside_state=1; rpdid=|(um|k~|J~kR0J'uYRY~lR|)k; CURRENT_QUALITY=0; CURRENT_BLACKGAP=0; PVID=6; innersign=1; bp_video_offset_1379528=635639736671666200; b_lsid=964A7CAD_17F6F2CB69E; CURRENT_FNVAL=80","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
+headers = {"Cookie":"","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
 
 def get_url():
-    data = pd.read_excel('D://桌面/b站原创top.xlsx', sheet_name='全区')
+    data = pd.read_excel('b站原创top.xlsx', sheet_name='全区')
     uid = list(data['up主ID'])
     return uid
 
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         list.append(dict)
         print(f'{url} 已完成')
     data = pd.DataFrame(list)
-    data.to_csv('D://桌面/top100up主.csv', encoding='utf_8_sig', index=False)
+    data.to_csv('top100up主.csv', encoding='utf_8_sig', index=False)
     print('--全部完成--')
 
 
