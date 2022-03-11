1a556dba2d2ea54fe59c869287a726a935e93564
menolzl <48782434+menolzl@users.noreply.github.com>
2022-03-12 02:51:08 +0800

    Update 3 danmuallin.py

--- 3 danmuallin.py
+++ 3 danmuallin.py
@@ -6,20 +6,23 @@ import datetime
 
 headers = {"Cookie":"","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
 
-
+# 获取需要抓取的日期
 def get_date():
     a = datetime.date(2022, 3, 8)
     b = datetime.date(2022, 3, 10)
     for i in range((b - a).days + 1):
         day = a + datetime.timedelta(days=i)
         yield day
-
+        
+        
+# 获取需要抓取的视频的BVID
 def get_bvid():
     data = pd.read_excel('b站原创top.xlsx',sheet_name='弹幕需求目录')
     bvid = list(data['BVID'])
     return bvid
 
 
+# 以BVID请求并获取对应的oid
 def get_oid(bvid):
     try:
         response = requests.get('https://api.bilibili.com/x/player/pagelist?bvid='+str(bvid)+'&jsonp=jsonp',headers=headers)
@@ -29,6 +32,7 @@ def get_oid(bvid):
         print('Error', e.args)
 
 
+# 请求弹幕数据
 def get_page(oid,dt):
     url = 'https://api.bilibili.com/x/v2/dm/web/history/seg.so?type=1&oid=' + str(oid) + '&date=' + str(dt)
 
@@ -39,26 +43,27 @@ def get_page(oid,dt):
     except requests.ConnectionError as e:
         print('Error', e.args)
 
+        
+# 将抓取结果按列拆分得到结果列表，正则每个元素的中文部分。
+def parse_page(response,bv):
 
-def parse_page(response):
-# 将抓取结果按列拆分得到结果列表，正则取每个元素的中文部分。
-    list = []   
+    list = ['bvid,弹幕']   
     result = response.split('\n')
     for i in result:
         pattern = re.compile('([\u4e00-\u9fa5]+)')
         data = re.findall(pattern,i)
         if data:
-            list.append(data[0])
+            list.append(f'{bv},{data[0]}')
     return list
 
 
 def save_data(data):
-    with open(f'弹幕/b站top20弹幕1.txt', 'a',encoding="utf_8_sig") as f:
+    with open(f'弹幕/视频弹幕.csv', 'a',encoding="utf_8_sig") as f:
         for i in data:
             f.write(i+'\n')
 
 
-
+# 先获取oid，拿到oid后，按照需要的日期循环请求获取弹幕并装入list中，所有请求完成后将list存入csv文件。
 if __name__ == '__main__':
     bvid = get_bvid()
     for bv in bvid:
@@ -66,7 +71,7 @@ if __name__ == '__main__':
         date = get_date()
         for dt in date:
             response =  get_page(oid,dt)
-            list = parse_page(response)
+            list = parse_page(response,bv)
             if list:
                 save_data(list)
             else:
