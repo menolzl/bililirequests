e7665b81db69e3a651d1a6d84a21cc46c5455735
menolzl <48782434+menolzl@users.noreply.github.com>
2022-03-12 03:16:50 +0800

    Update 3 danmuallin.py

--- 3 danmuallin.py
+++ 3 danmuallin.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import re
 import datetime
+import os
 
 headers = {"Cookie":"","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
 
